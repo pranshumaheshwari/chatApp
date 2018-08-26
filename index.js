bdb36129121668173e02294cf4874b56b3c86c3d
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2018-08-26 13:50:29 +0530

    Added folder structure and initialized npm

---
+++ index.js
@@ -0,0 +1,23 @@
+//             DEPENDENCIES
+var express 	   = require('express'),
+	bodyParser 	   = require('body-parser'),
+	methodOverride = require('method-override'),
+  app 		       = express(),
+  http           = require('http').Server(app),
+  io             = require('socket.io')(http);
+
+//              SETUP
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+  app.use(methodOverride("_method"));
+  app.use(express.static( __dirname + "/public"));
+  app.set("view engine","html");
+
+//             ROOT
+  app.get("/",function(req,res){
+    res.send("<h1>Workinig</h1>");
+  });
+
+//            LISTEN
+  app.listen(3000,function(){
+    console.log("Server started at port 3000!");
+  });
